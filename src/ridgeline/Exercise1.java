1f67b3bcbea5886b5246125395ebdbdf93377132
Filipe Abalada <filipe.abalada@gmail.com>
2026-05-21 21:17:44 -0400

    Updated Ridgeline 2026

--- src/ridgeline/Exercise1.java
+++ src/ridgeline/Exercise1.java
@@ -1,84 +1,85 @@
 package ridgeline;
 
-/*
-
-    ChatGPT has predicted future prices for stocks in your portfolio.  You decide to build a portfolio
-    accounting tool to track your portfolio and generate trades based on the predictions.
-
-    Step 1 - Accounting:
-    Build a class / object that can track your portfolio.  You don’t need to track individual trades, but
-    it should capture the net impact of the trade on the portfolio (+/- cash and +/- quantity of shares).
-
-    It should have methods:
-        * Buy(ticker, quantity, pricePerShare) : void : exception
-           - Must have sufficient cash
-        * Sell(ticker, quantity, pricePerShare) : void : exception
-           - Must have sufficient quantity
-        * GetCashBalance() : decimal
-        * GetQuantityOfShares(ticker) : integer
-
- */
-
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *** Exact same interview in 2023 and in May 21st, 2026 (Jeffersonton, VA) ***
+ *
+ * Step 1 - Accounting (limit 15 minutes):
+ * Build a class / object that can track your portfolio.  You do not need to track individual trades or tax lots,
+ * but it should capture the net impact of the trade on the portfolio (+/- cash and +/- quantity of shares).
+ *
+ * It should have methods:
+ *     * Buy(ticker: string, quantity: integer, pricePerShare: decimal) : void : exception
+ *        - Must have sufficient cash
+ *     * Sell(ticker: string, quantity: integer, pricePerShare: decimal) : void : exception
+ *        - Must have sufficient quantity
+ *     * GetCashBalance() : decimal
+ *     * GetQuantityOfShares(ticker: string) : integer
+ */
 public class Exercise1 {
+
+    static class Accounting {
+        private double cashBalance;
+        private Map<String, Integer> portfolio;
+
+        public Accounting(double initialCashBalance) {
+            if (initialCashBalance > 0) {
+                cashBalance = initialCashBalance;
+            }
+            portfolio = new HashMap<>();
+        }
+
+        public void buy(String ticker, Integer qty, double pricePerShare) {
+            double buyingAmount = pricePerShare * qty;
+
+            if (cashBalance >= buyingAmount) {
+                cashBalance -= buyingAmount;
+                portfolio.put(ticker, portfolio.getOrDefault(ticker, 0) + qty);
+            }
+        }
+
+        public void sell(String ticker, Integer qty, double pricePerShare) {
+            int tickerQty = portfolio.getOrDefault(ticker, 0);
+
+            if (tickerQty >= qty) {
+                portfolio.put(ticker, tickerQty - qty);
+                cashBalance += qty * pricePerShare;
+            }
+        }
+
+        public double getCashBalance() {
+            return cashBalance;
+        }
+
+        public int getQuantityOfShares(String ticker) {
+            return portfolio.getOrDefault(ticker, 0);
+        }
+    }
+
     public static void main(String[] args) {
-        Accounting acc = new Accounting(10000d);
-        System.out.println("Initial cashBalance: " + acc.getCashBalance());
+        // 2023
+        Accounting accounting = new Accounting(10000d);
+        System.out.println("Initial cashBalance: " + accounting.getCashBalance());
 
-        acc.buy("AAPL", 50, 150);
-        System.out.println("AAPL: " + acc.getQuantityOfShares("AAPL"));
-        System.out.println("cashBalance: " + acc.getCashBalance());
+        accounting.buy("AAPL", 50, 150);
+        System.out.println("AAPL: " + accounting.getQuantityOfShares("AAPL"));
+        System.out.println("cashBalance: " + accounting.getCashBalance());
 
-        acc.sell("AAPL", 10, 300);
-        System.out.println("AAPL: " + acc.getQuantityOfShares("AAPL"));
-        System.out.println("cashBalance: " + acc.getCashBalance());
+        accounting.sell("AAPL", 10, 300);
+        System.out.println("AAPL: " + accounting.getQuantityOfShares("AAPL"));
+        System.out.println("cashBalance: " + accounting.getCashBalance());
+
+        // 2026
+        accounting = new Accounting(500d);
+
+        accounting.buy("TSLA", 10, 25d);
+        System.out.println(accounting.getCashBalance()); //250
+        System.out.println(accounting.getQuantityOfShares("TSLA")); //10
+
+        accounting.sell("TSLA", 5, 30d);
+        System.out.println(accounting.getCashBalance()); //400
+        System.out.println(accounting.getQuantityOfShares("TSLA")); //5
     }
-}
-
-class Accounting {
-
-    private double cashBalance;
-
-    private Map<String, Integer> portfolio;
-
-    public Accounting(double initialCashBalance) {
-        cashBalance = initialCashBalance;
-        portfolio = new HashMap<>();
-    }
-
-    public void buy(String ticker, Integer qty, double pricePerShare) {
-        double buyingPrice = pricePerShare * qty;
-
-        if (cashBalance >= buyingPrice) {
-            cashBalance -= buyingPrice;
-        }
-
-        Integer qtyTicker = portfolio.get(ticker);
-        if (qtyTicker == null) {
-            portfolio.put(ticker, qty);
-        } else {
-            portfolio.put(ticker, qtyTicker+qty);
-        }
-    }
-
-    public void sell(String ticker, Integer qty, double pricePerShare) {
-        int tickerQty = portfolio.get(ticker);
-
-        if (tickerQty >= qty) {
-            portfolio.put(ticker, tickerQty - qty);
-        }
-
-        cashBalance += qty * pricePerShare;
-    }
-
-    public double getCashBalance() {
-        return cashBalance;
-    }
-
-    public Integer getQuantityOfShares(String ticker) {
-        return portfolio.get(ticker);
-    }
-
 }
--- src/ridgeline/Exercise2.java
+++ src/ridgeline/Exercise2.java
@@ -1,76 +1,100 @@
 package ridgeline;
 
-import java.util.Arrays;
-
-/*
-
-    ChatGPT has predicted future prices for stocks in your portfolio.  You decide to build a portfolio
-    accounting tool to track your portfolio and generate trades based on the predictions.
-
-    Step 2 - Trading:
-    Write code that generates trades to maximize gains in a single stock.  You may trade at most
-    once per day, but are not required to trade every day.  Shares must be purchased as whole
-    numbers (no fractional shares).  Shares and cash cannot be negative.
-
-    Using code from Step 1 may be helpful, but is not required.
-
-    Starting Cash: 100000
-    Ticker: ABCD
-
-    Predicted Stock Price:
-    Day 1: 170
-    Day 2: 175
-    Day 3: 172
-    Day 4: 178
-    Day 5: 180
-
-    Output:
-        * Daily Activity
-            Format: (BUY|SELL|HOLD) 100 Shares ABCD at $170 : cash balance $1000
-        * BONUS: the ending portfolio value
-            Format: Ending Value: $123456 (23.46%)
-
+/**
+ *** Exact same interview in 2023 and in May 21st, 2026 (Jeffersonton, VA) ***
+ *
+ * Step 2 - Trading (limit 30 minute):
+ * Given a list of predicted daily prices for a stock, write code that generates trades to maximize gains in that stock.
+ *
+ * Instructions:
+ * You may trade at most once per day, but are not required to trade every day.
+ * Shares must be purchased as whole numbers (no fractional shares).
+ * Shares and cash cannot be negative.
+ * The position should be liquidated when there is no remaining price data.
+ * Log what actions are taken each day (see “Output” below).
+ * There are multiple ways to solve this problem!
+ *
+ * Using code from Step 1 may be helpful, but is not required.
+ *
+ * Starting Cash: 100_000
+ * Ticker: ABCD
+ *
+ * Predicted Daily Prices for ABCD:
+ *  [
+ *   170, // Day 1
+ *   175, // Day 2
+ *   172, // Day 3
+ *   178, // Day 4
+ *   180 // Day 5
+ *  ]
+ *
+ * Output:
+ *     * Daily Activity
+ *         Format: (BUY|SELL|HOLD) 100 Shares ABCD at $170 : cash balance $1000
+ *     * BONUS: the ending portfolio value
+ *         Format: Ending Value: $123456 (23.46%)
  */
 public class Exercise2 {
 
-    public static enum Order {
-        BUY,
-        SELL,
-        HOLD;
+    /**
+     * The pattern here is a classic greedy with state.
+     * Buy at local minimum, hold until the peak before a drop or end of data, then sell.
+     *
+     * For [170, 175, 172, 178, 180]:
+     *
+     * Buy day 1 at 170
+     * Day 2 is 175 — higher, but day 3 drops to 172, so sell day 2 at 175
+     * Buy day 3 at 172
+     * Day 4 is 178, day 5 is 180 — keep rising, sell day 5 at 180
+     *
+     * - if not holding AND prices[i] < prices[i+1] → BUY
+     * - if holding AND (prices[i] > prices[i+1] OR last day) → SELL
+     * - else → HOLD
+     *
+     */
+    public static int maxProfit(int initialCash, int[] prices) {
+        StringBuilder sb = new StringBuilder();
+        int cash=initialCash;
+        boolean hold = false;
+        int numberOfShares = 0;
+
+        for (int i=0; i<prices.length; i++) {
+            // CAN BUY
+            if (!hold && i<prices.length-1 && prices[i] < prices[i+1]) {
+                numberOfShares = cash / prices[i];
+                cash -= numberOfShares * prices[i];
+                sb.append("BUY " + numberOfShares + " Shares ABCD at $" + prices[i] + " : cash balance $" + cash + "\n");
+                hold = true;
+            }
+            // CAN SELL
+            else if (hold && ((i < prices.length-1 && prices[i] > prices[i+1]) || i == prices.length-1)) {
+                cash += numberOfShares * prices[i];
+                sb.append("SELL " + numberOfShares + " Shares ABCD at $" + prices[i] + " : cash balance $" + cash + "\n");
+                hold = false;
+                numberOfShares = 0;
+            }
+            // HOLD
+            // holding and price still rising, or not holding and price falling, or not holding, price raising on last day
+            else {
+                sb.append("HOLD " + numberOfShares + " Shares ABCD at $" + prices[i] + " : cash balance $" + cash + "\n");
+            }
+        }
+
+        System.out.println(sb);
+        System.out.println("$" + cash + " (%.1f%%)".formatted((double)(cash-initialCash)/initialCash*100));
+
+        return cash;
     }
 
-    public static void trade(int[] prices) {
-        int [][] dp = new int[prices.length][2];
-        Order[] seq = new Order[prices.length];
-        System.out.println(f(0, prices, dp, seq, 1));
-        Arrays.stream(seq).toList().forEach(System.out::println);
-    }
-
-    public static int f(int i, int[] prices, int[][] dp, Order[] seq, int canBuy) {
-        if (i == prices.length) {
-            return 0;
-        }
-        if (dp[i][canBuy]>0) {
-            return dp[i][canBuy];
-        }
-        if (canBuy == 1) {
-            int buy = -prices[i] + f(i + 1, prices, dp, seq, 0);
-            int hold = f(i + 1, prices, dp, seq, 1);
-
-            seq[i] = (buy > hold) ? Order.BUY : Order.HOLD;
-            dp[i][canBuy] = Math.max(buy, hold);
-        } else {
-            int sell = prices[i] + f(i + 1, prices, dp, seq, 1);
-            int hold = f(i + 1, prices, dp, seq, 0);
-
-            seq[i] = (sell > hold) ? Order.SELL : Order.HOLD;
-            dp[i][canBuy] = Math.max(sell, hold);
-        }
-        return dp[i][canBuy];
-    }
-
+    /**
+     * Fixed Solution in 2026 and removed the DP version that was buggy.
+     */
     public static void main(String[] args) {
-        int[] ex1 = {170, 175, 172, 178, 180};
-        trade(ex1);
+        int[] prices = {170, 175, 172, 178, 180};
+        maxProfit(100000, prices);
+
+        System.out.println();
+        prices = new int[] {170, 172, 175};
+        maxProfit(100000, prices);
     }
 }
